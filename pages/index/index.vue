0d0e5dc0268c64f7c3a494eadf7d859a349de635
语默 <smartwsg@163.com>
2019-12-03 22:52:49 +0800

    项目初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="content">
+		12
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+		},
+		onLoad() {
+				uni.navigateTo({
+					url: '../home/home'
+				});
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
