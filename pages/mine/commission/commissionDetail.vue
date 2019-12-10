2aa35640741f0b097b5e4cc41ac5ddfa4ab129bd
语默 <smartwsg@163.com>
2019-12-11 00:15:22 +0800

    -m‘佣金页面增加

---
+++ pages/mine/commission/commissionDetail.vue
@@ -0,0 +1,142 @@
+<template>
+	<view class="commissionDetail">
+		<view class="notice">
+			每月25日可提现上月【确认收货】的收益
+		</view>
+		<view class="commissionDetail_content">
+			<view class="title">
+				<text>日预估收入</text>
+				<view class="title_btn" @click="toDo('allOrder')">查看全部<view class="iconfont iconyou"></view></view>
+			</view>
+			<view class="commissionBox" v-for="(item,index) of dayMoney" :key='index'> 
+				<view class="commissionBox_List">
+					<view class="commissionBox_Item" :class="index" v-for="(val,idx) of item" :key="index+'_'+idx">
+						<view>{{ val['k'] }}</view>
+						<view >{{ val['v'] }}</view>
+					</view>
+				</view>
+			</view>
+			<view class="title">
+				<text>月预估收入</text>
+				<view class="title_btn" @click="toDo('allOrder')">查看全部<view class="iconfont iconyou"></view></view>
+			</view>
+			<view class="commissionBox">
+				<view class="commissionBox_List"  v-for="(item,index) of mouthData" :key='index'>
+					<view class="commissionBox_Item" :class="index" v-for="(val,idx) of item" :key="index+'_'+idx">
+						<view>{{ val['k'] }}</view>
+						<view >{{ val['v'] }}</view>
+					</view>
+				</view>
+			</view>
+			<view class="commissionBox">
+				<view class="title">
+					<text>月预估收入</text>
+					<view class="title_btn" @click="toDo('allOrder')">查看全部<view class="iconfont iconyou"></view></view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data(){
+			return {
+				dayMoney:{
+					tody:{
+						num:{
+							k:'付款笔数',
+							v:'0'
+						},
+						chengjiao:{
+							k:'成交预估收入',
+							v:'0'
+						},
+						jiesuan:{
+							k:'结算预估收入',
+							v:'0'
+						}
+					},
+					tomorrow:{
+						num:{
+							k:'付款笔数',
+							v:'0'
+						},
+						chengjiao:{
+							k:'成交预估收入',
+							v:'0'
+						},
+						jiesuan:{
+							k:'结算预估收入',
+							v:'0'
+						}
+					}
+				},
+				mouthData:{
+					nowMouth:{
+						chengjiao:{
+							k:'本月成交预估收入',
+							v:'0'
+						},
+						jiesuan:{
+							k:'本月结算预估收入',
+							v:'0'
+						}
+					},
+					prevMouth:{
+						chengjiao:{
+							k:'上月成交预估收入',
+							v:'0'
+						},
+						jiesuan:{
+							k:'上月结算预估收入',
+							v:'0'
+						}
+					}
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="less">
+	@groveWidth: 20upx;
+	.commissionDetail{
+		background: #EDEDED;
+		.commissionDetail_content{
+			margin: @groveWidth;
+		}
+	}
+	.title {
+		padding: .5rem 0;
+		display: flex;
+		justify-content: space-between;
+	
+		.title_btn {
+			color: #a7a7a7;
+			display: flex;
+			align-items: center;
+		}
+	}
+	.commissionBox{
+		background: #FFFFFF;
+		border-radius: 10upx;
+		padding: 0 @groveWidth;
+		.commissionBox_List{
+			display: flex;
+			padding: @groveWidth 0;
+			.commissionBox_Item{
+				color: #969696;
+				flex: 1;
+				text-align: center;
+				& + .commissionBox_Item{
+					border-left: 1upx solid #969696;
+				}
+			}
+		}
+		&+.commissionBox{
+			margin-top: 20upx;
+		}
+		
+	}
+</style>
---
+++ pages/mine/commission/index.vue
@@ -0,0 +1,115 @@
+<template>
+	<!-- 佣金页面 -->
+	<view class="commission">
+		<!-- 账户余额 -->
+		<view class="balance">
+			<text class="labelText">账号余额：</text>
+			<view class="balance_money">
+				<text>{{ money }}</text>
+				<view class="tx">
+					去提现
+					<view class="iconfont iconyou"></view>
+				</view>
+			</view>
+		</view>
+		<!-- tabs -->
+		<QSTabs
+		ref="tabs" 
+		:tabs="tabs" 
+		animationMode="line3" 
+		backgroundColor="#010101"
+		defaultColor="#fff"
+		:current="current" 
+		@change="change"
+		activeColor="#F8D590"
+		lineColor="#F8D590"
+		height="80"
+		swiperWidth="750">
+		</QSTabs>
+		<!-- tabs内页 -->
+		<swiper
+		:style="{'height': '1000rpx'}" 
+		:current="swiperCurrent" 
+		@transition="transition"
+		@animationfinish="animationfinish">
+			<swiper-item class="swiper-item" v-for="(item, index) in tabs" :key="index">
+				<scroll-view scroll-y style="height: 100%;width: 100%;" >
+					<commissionDetail></commissionDetail>
+				</scroll-view>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	import QSTabs from '@/components/QS-tabs/QS-tabs.vue';
+	import commissionDetail from './commissionDetail';
+		export default {
+			components: {
+				QSTabs,commissionDetail
+			},
+			data(){
+				return{
+					current: 0,
+					swiperCurrent: 0,
+					tabsHeight: 0,
+					dx: 0,
+					tabs:[
+						{name:'京东',key:'all'},
+						{name:'淘宝',key:'all'},
+						{name:'拼多多',key:'all'}
+					],
+					money:0
+				}
+			},
+			methods: {
+				toHome(){
+					uni.switchTab({
+					    url: '/pages/home/index'
+					});
+				},
+				change(index) {
+					this.swiperCurrent = index;
+				},
+				transition({ detail: { dx } }) {
+					this.$refs.tabs.setDx(dx);
+				},
+				animationfinish({detail: { current }}) {
+					this.$refs.tabs.setFinishCurrent(current);
+					this.swiperCurrent = current;
+					this.current = current;
+				}
+			}
+		}
+</script>
+
+<style lang="less">
+	.commission{
+		.balance{
+			background: #010101;
+			color: #fff;
+			.labelText{
+				color: #797979;
+			}
+			.balance_money{
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				margin-top: 30upx;
+				uni-text{
+					font-size: 50upx;
+				}
+				.tx{
+					display: flex;
+					align-items: center;
+					background-image: linear-gradient(to right, #F9DA9E , #F7D48D);
+					padding: 0 25upx;
+					font-size: 20upx;
+					color: #7F4F18;
+					border-radius: 14px  0 0 14px;
+				}
+			}
+		}
+	}
+	
+</style>
